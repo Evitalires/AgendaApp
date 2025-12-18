9264e9433c703d0b22142a1855724e5240e58359
Evitalires <evitalires@gmail.com>
2025-12-18 11:55:09 -0500

    Implementando los getter and setter de Contacto

--- src/Contacto.java
+++ src/Contacto.java
@@ -1,11 +1,38 @@
 public class Contacto {
     String nombre;
+    String apellido;
     String celular;
 
-    public Contacto(String nombre, String celular) {
+    public Contacto(String nombre, String apellido, String celular) {
         this.nombre = nombre;
+        this.apellido = apellido;
         this.celular = validarFormatoCelular(celular);
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = validarFormatoCelular(celular);
+    }
+
     String validarFormatoCelular(String celular){
         //Celular
         String primerNumero = celular.substring(0,1);
